abedc2be3fb31fba75dc3f201dd2327277af618e
MirzaMaulana <mirzaartstudio35@gmail.com>
2023-07-21 10:40:52 +0700

    interface inheritance

--- bundle/interface.js
+++ bundle/interface.js
@@ -1,4 +1,8 @@
 "use strict";
+/**
+ * Interface
+ * adalah cara untuk mendefinisikan bentuk atau struktur dari sebuah objek
+ */
 const teman = {
     name: "Orlombus",
     age: 21,
@@ -8,3 +12,26 @@ const teman = {
     },
 };
 const result2 = teman.sayHello();
+function createMotor(motor) {
+    console.log(`
+        --------------------
+        Membuat Motor..
+        --------------------
+        Motor berhasil dibuat, dengan detail berikut
+        nama motor = ${motor.name}
+        roda = ${motor.roda}
+        warna = ${motor.warna}
+        tahun = ${motor.tahun}
+        type = ${motor.type}
+        bensin = ${motor.bensin}
+        `);
+}
+const Beat = {
+    roda: 2,
+    warna: "Hitam",
+    tahun: 2023,
+    name: "Beat 2x02",
+    type: "Matic",
+    bensin: "Pertalite",
+};
+createMotor(Beat);
--- src/interface.ts
+++ src/interface.ts
@@ -20,3 +20,47 @@ const teman: Orang = {
 };
 
 const result2 = teman.sayHello();
+
+/**
+ * interface inheritance
+ */
+
+interface Kendaraan {
+  roda: number;
+  warna: string;
+  tahun: number;
+}
+
+interface Motor extends Kendaraan {
+  name: string;
+  type: string;
+  bensin: string;
+}
+
+function createMotor(motor: Motor): void {
+  console.log(
+    `
+        --------------------
+        Membuat Motor..
+        --------------------
+        Motor berhasil dibuat, dengan detail berikut
+        nama motor = ${motor.name}
+        roda = ${motor.roda}
+        warna = ${motor.warna}
+        tahun = ${motor.tahun}
+        type = ${motor.type}
+        bensin = ${motor.bensin}
+        `
+  );
+}
+
+const Beat = {
+  roda: 2,
+  warna: "Hitam",
+  tahun: 2023,
+  name: "Beat 2x02",
+  type: "Matic",
+  bensin: "Pertalite",
+};
+
+createMotor(Beat);
