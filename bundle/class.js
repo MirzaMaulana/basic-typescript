995909204e596d5ff51349f2d8e8cd7ae156bc5a
MirzaMaulana <mirzaartstudio35@gmail.com>
2023-07-21 13:41:00 +0700

    class inheritance

--- bundle/class.js
+++ bundle/class.js
@@ -11,5 +11,30 @@ class Mouse {
         console.log(`Mouse ${this.merk} Berhasil Dibuat dengan type ${this.type} `);
     }
 }
-const rexus = new Mouse("Rexus", "Gamin");
+const rexus = new Mouse("Rexus", "Gaming");
 rexus.create();
+const logitech = new Mouse("Logitech", "Wireless");
+logitech.create();
+/**
+ * class inheritance di typescript
+ */
+class Animal {
+    constructor(name, type) {
+        this.name = name;
+        this.type = type;
+    }
+    detail() {
+        console.log(`Hewan ini namanya ${this.name} typenya ${this.type}`);
+    }
+}
+class Kucing extends Animal {
+    constructor(name, type, suara) {
+        super(name, type);
+        this.suara = suara;
+    }
+    meong() {
+        console.log(`${this.name} adalah hewan ${this.type} suaranya ${this.suara}`);
+    }
+}
+const kucing = new Kucing("Kucing", "Karnivora", "Meong");
+kucing.meong();
--- src/class.ts
+++ src/class.ts
@@ -14,5 +14,44 @@ class Mouse {
   }
 }
 
-const rexus = new Mouse("Rexus", "Gamin");
+const rexus = new Mouse("Rexus", "Gaming");
 rexus.create();
+
+const logitech = new Mouse("Logitech", "Wireless");
+logitech.create();
+
+/**
+ * class inheritance di typescript
+ */
+
+class Animal {
+  name: string;
+  type: "Herbivora" | "Karnivora" | "Omnivora";
+  constructor(name: string, type: "Herbivora" | "Karnivora" | "Omnivora") {
+    this.name = name;
+    this.type = type;
+  }
+  detail() {
+    console.log(`Hewan ini namanya ${this.name} typenya ${this.type}`);
+  }
+}
+
+class Kucing extends Animal {
+  suara: string;
+  constructor(
+    name: string,
+    type: "Herbivora" | "Karnivora" | "Omnivora",
+    suara: string
+  ) {
+    super(name, type);
+    this.suara = suara;
+  }
+  meong() {
+    console.log(
+      `${this.name} adalah hewan ${this.type} suaranya ${this.suara}`
+    );
+  }
+}
+
+const kucing = new Kucing("Kucing", "Karnivora", "Meong");
+kucing.meong();
